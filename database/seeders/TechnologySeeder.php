fcc44545a2ad901e6a48de5f0449b3b502745d91
giovannitamburini <giovatambu@gmail.com>
2023-05-24 15:22:24 +0200

    added migration & seeder of Technology. added migration of project_technology table

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,15 @@
 
 namespace Database\Seeders;
 
+// technology model
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+// Faker
+use Faker\Generator as Faker;
+//devo fare l'import del dell'helper e in questo caso specifico per le stringhe
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,8 +19,21 @@ class TechnologySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $technologies = ['HTML', 'CSS', 'Figma', 'JavaScript', 'Postman', 'MySQL', 'PHP', 'Sass', 'VS Code'];
+
+        foreach ($technologies as $technology) {
+
+            $newTechnolgy = new Technology();
+
+            $newTechnolgy->name = $technology;
+            // uso il metodo slug per convertire il name in slug
+            $newTechnolgy->slug = Str::slug($newTechnolgy->name, '-');
+            // uso il faker per generare i colori casualmente
+            $newTechnolgy->color = $faker->hexColor();
+
+            $newTechnolgy->save();
+        }
     }
 }
